65157323e22a6fa53424becf3e4ee44cf000b766
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-26 17:19:06 +0530

    Update counter-view.js

---
+++ src/views/counter-view.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Segment } from "semantic-ui-react";
+
+import { CounterContextProvider } from "../context/counter-context";
+import CounterDisplay from "../components/counter-display";
+import CounterButtons from "../components/counter-buttons";
+
+export default function CounterView() {
+    return (
+        
+    );
+}
