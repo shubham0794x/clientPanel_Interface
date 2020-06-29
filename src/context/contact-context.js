9b57f3435c94886a148f2683ad27cd6c9cf2cbc5
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-29 20:05:34 +0530

    Update contact-context.js

--- src/context/contact-context.js
+++ src/context/contact-context.js
@@ -24,3 +24,27 @@ const initialState = {
     error: null
 };
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_CONTACT":
+            return {
+                contacts: [...state.contacts, action.payload]
+            };
+        case "DEL_CONTACT":
+            return {
+                contacts: state.contacts.filter(
+                    contact => contact.id !== action.payload
+                )
+            };
+        case "START":
+            return {
+                loading: true
+            };
+        case "COMPLETE":
+            return {
+                loading: false
+            };
+        default:
+            throw new Error();
+    }
+};
