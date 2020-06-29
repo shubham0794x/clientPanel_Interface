36f880d09df067927bfda3408ff6f3a063a8d6eb
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-29 20:08:08 +0530

    Update contact-view.js

---
+++ src/views/contact-view.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Segment, Header } from "semantic-ui-react";
+import ContactForm from "../components/contact-form";
+import ContactTable from "../components/contact-table";
+import { ContactContextProvider } from "../context/contact-context";
+
+export default function Contacts() {
+    return (
+       
+    );
+}
